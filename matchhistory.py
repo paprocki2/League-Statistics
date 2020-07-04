96087a0e7f76c0d9a7cd4e23ecf2902723cf4cca
paprocki2 <alexpaprocki1@gmail.com>
2020-07-04 16:04:37 -0500

    Initial commit

---
+++ matchhistory.py
@@ -0,0 +1,29 @@
+from summoneracc import lol_watcher, region, me
+import pandas
+
+my_matches = lol_watcher.match.matchlist_by_account(region, me['accountId'])
+
+# fetch last match detail
+last_match = my_matches['matches'][0]
+match_detail = lol_watcher.match.by_id(region, last_match['gameId'])
+
+participants = []
+for row in match_detail['participants']:
+    participants_row = {}
+    participants_row['champion'] = row['championId']
+    participants_row['spell1'] = row['spell1Id']
+    participants_row['spell2'] = row['spell2Id']
+    participants_row['win'] = row['stats']['win']
+    participants_row['kills'] = row['stats']['kills']
+    participants_row['deaths'] = row['stats']['deaths']
+    participants_row['assists'] = row['stats']['assists']
+    participants_row['totalDamageDealt'] = row['stats']['totalDamageDealt']
+    participants_row['goldEarned'] = row['stats']['goldEarned']
+    participants_row['champLevel'] = row['stats']['champLevel']
+    participants_row['totalMinionsKilled'] = row['stats']['totalMinionsKilled']
+    participants_row['item0'] = row['stats']['item0']
+    participants_row['item1'] = row['stats']['item1']
+    participants.append(participants_row)
+df = pandas.DataFrame(participants)
+df
+print(participants)
