60717495066ad60b88e0cf90c0579964511b9d95
paprocki2 <alexpaprocki1@gmail.com>
2020-08-02 16:48:17 -0500

    Added get_champion and get_champ_dict

--- summoneracc.py
+++ summoneracc.py
@@ -3,20 +3,46 @@ from settings import API_KEY, SUMMONER_NAME, DB_IP, DB_PASS
 import pandas as pd
 import mysql.connector
 
+region = 'na1'
 def connect_to_database():
      connector = mysql.connector.connect(user='root', password=DB_PASS, host=DB_IP, database='Matches')
      cursor = connector.cursor()
      return cursor
 
-def get_summoner():
+def get_watcher():
      lol_watcher = LolWatcher(API_KEY)
-     region = 'na1'
-     ddragon = LolWatcher.data_dragon
-     ddragon
-     me = lol_watcher.summoner.by_name(region, SUMMONER_NAME)
-     print(me)
+     return lol_watcher 
 
-def print_ranked_stats(region, summoner, lol_watcher):
+def get_ddragon():
+     ddragon = LolWatcher.data_dragon
+     return ddragon
+
+def get_summoner(lol_watcher):
+     summoner = lol_watcher.summoner.by_name(region, SUMMONER_NAME)
+     print(summoner)
+     return summoner
+
+def get_champ_dict(lol_watcher):
+     # check league's latest version
+     latest = lol_watcher.data_dragon.versions_for_region(region)['n']['champion']
+     # Lets get some champions static information
+     static_champ_list = lol_watcher.data_dragon.champions(latest, False, 'en_US')
+
+     # champ static list data to dict for looking up
+     champ_dict = {}
+     for key in static_champ_list['data']:
+          row = static_champ_list['data'][key]
+          champ_dict[row['key']] = row['id']
+     return champ_dict
+
+def get_champion(champ_dict, participants):
+     print(champ_dict)
+     print(champ_dict['69'])
+     for row in participants:
+          print(str(row['champion']) + ' ' + champ_dict[str(row['champion'])])
+          """ row['championName'] = champ_dict[str(row['champion'])] """
+
+def print_ranked_stats(lol_watcher, summoner, region):
      my_ranked_stats = lol_watcher.league.by_summoner(region, summoner['id'])
      print(my_ranked_stats)
 
@@ -24,6 +50,7 @@ def get_matchlist(lol_watcher, summoner, region):
      my_matches = lol_watcher.match.matchlist_by_account(region, summoner['accountId'], None, None, None, 1)
      # fetch last match detail
      last_match = my_matches['matches'][0]
+     print(last_match)
      print(last_match)
      match_detail = lol_watcher.match.by_id(region, last_match['gameId'])
      participants = []
@@ -48,7 +75,18 @@ def get_matchlist(lol_watcher, summoner, region):
           participants_row['item5'] = row['stats']['item5']
           participants_row['item6'] = row['stats']['item6']
           print(row['stats']['item4'])
-     
           participants.append(participants_row)
      df = pd.DataFrame(participants)
      print(df)
+     return participants
+def main():
+     cursor = connect_to_database()
+     lol_watcher = get_watcher()
+     summoner = get_summoner(lol_watcher)
+     participants = get_matchlist(lol_watcher, summoner, region)
+     ddragon = get_ddragon()
+
+     get_champion(get_champ_dict(lol_watcher), participants)
+
+if __name__ == "__main__":
+     main()
