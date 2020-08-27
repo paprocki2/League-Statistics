8eae8a20ab14a0bfe31d097ad9902b86b1cd7e45
paprocki2 <alexpaprocki1@gmail.com>
2020-08-26 20:21:21 -0500

    Changes/refactoring

--- summoneracc.py
+++ summoneracc.py
@@ -4,10 +4,20 @@ import pandas as pd
 import mysql.connector
 
 region = 'na1'
-def connect_to_database():
+def add_to_database(stats):
      connector = mysql.connector.connect(user='root', password=DB_PASS, host=DB_IP, database='Matches')
-     cursor = connector.cursor()
-     return cursor
+     cur = connector.cursor()
+     insert = """INSERT INTO Matches(gameId, participantId, summoner, champion, spell1, spell2, win, kills, deaths, assists, 
+     level, minionsKilled, item0, item1, item2, item3, item4, item5, trinket) 
+     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+     for row in stats:
+          summonerData = (row['gameId'], row['participantId'], row['summoner'], row['champion'], row['spell1'], row['spell2'],
+          row['win'], row['kills'], row['deaths'], row['assists'], row['level'], row['minionsKilled'], row['item0'],
+          row['item1'], row['item2'], row['item3'], row['item4'], row['item5'], row['trinket'])
+          cur.execute(insert, summonerData)
+     print("success")
+     connector.commit()
+     return cur
 
 def get_watcher():
      lol_watcher = LolWatcher(API_KEY)
@@ -21,12 +31,10 @@ def get_summoner(lol_watcher):
 def get_spell_dict(lol_watcher):
      latest = lol_watcher.data_dragon.versions_for_region(region)['n']['summoner']
      static_spells_list = lol_watcher.data_dragon.summoner_spells(latest, 'en_US')
-     print(static_spells_list)
      spell_dict = {}
      for key in static_spells_list['data']:
           row = static_spells_list['data'][key]
           spell_dict[row['key']] = row['name']
-     print(spell_dict)
      return spell_dict
 
 def get_item_dict(lol_watcher):
@@ -48,6 +56,9 @@ def get_champ_dict(lol_watcher):
      #print(champ_dict)
      return champ_dict
 
+def get_spell(spell_dict, spellId):
+     return spell_dict[str(spellId)]
+
 def get_item(item_dict, itemId):
      if itemId == 0:
           return "N/A"
@@ -56,99 +67,84 @@ def get_item(item_dict, itemId):
 def get_champion(champ_dict, championId):
      return champ_dict[str(championId)]
 
-def get_champion1(champ_dict, participants):
-     print(champ_dict)
-     print(champ_dict['69'])
-     for row in participants:
-          print(str(row['champion']) + ' ' + champ_dict[str(row['champion'])])
-          """ row['championName'] = champ_dict[str(row['champion'])] """
-
 def print_ranked_stats(lol_watcher, summoner, region):
      my_ranked_stats = lol_watcher.league.by_summoner(region, summoner['id'])
      print(my_ranked_stats)
 
-def get_matchlist(lol_watcher, summoner, region, champ_dict, item_dict):
+def get_matchlist(lol_watcher, region, summoner, index):
+     my_matchlist = lol_watcher.match.matchlist_by_account(region, summoner['accountId'], None, 1578477600, None, index)
+     return my_matchlist 
+
+def get_match(lol_watcher, my_matchlist, champ_dict, item_dict, spell_dict):
      #pd.set_option("display.max_rows", None, "display.max_columns", None)
      pd.set_option('display.max_rows', None)
      pd.set_option('display.max_columns', None)
      pd.set_option('display.width', None)
      pd.set_option('display.max_colwidth', 40)
-     my_matches = lol_watcher.match.matchlist_by_account(region, summoner['accountId'], None, None, None, 1)
-     # fetch last match detail
-     last_match = my_matches['matches'][0]
-     #print(last_match)
+     last_match = my_matchlist['matches'][0]
      match_detail = lol_watcher.match.by_id(region, last_match['gameId'])
      gameId = match_detail['gameId']
      participants = []
      stats = []
-     full = []
-     #print(match_detail['participantIdentities']['participantId'])
-     """ for row in match_detail:
-          participants1 = row['participants']
-          participantIdentities = row['participantIdentities']
-          participants_row = {}
-          participants_row['participantId'] = participantIdentities['participantId']
-          player = participantIdentities['player']
-          participants_row['summonerName'] = player['summonerName']
-          participants_row['champion'] = get_champion(champ_dict, participants1['championId'])
-          participants_row['spell1'] = participants1['spell1Id']
-          participants_row['spell2'] = participants1['spell2Id']
-          participants_row['win'] = participants1['stats']['win']
-          participants_row['kills'] = participants1['stats']['kills']
-          participants_row['deaths'] = participants1['stats']['deaths']
-          participants_row['assists'] = participants1['stats']['assists']
-          full.append(participants_row) """
      for row in match_detail['participantIdentities']:
           participants_row = {}
           participants_row['gameId'] = gameId
           participants_row['participantId'] = row['participantId']
-          print(row)
           player = row['player']
-          
           participants_row['summonerName'] = player['summonerName']
           participants.append(participants_row)
-     
+     print(participants)
      for row in match_detail['participants']:
           participants_row = {}
           participants_row['gameId'] = gameId
           participants_row['participantId'] = row['stats']['participantId']
+          participants_row['summoner'] = participants[participants_row['participantId'] - 1]['summonerName']
           participants_row['champion'] = get_champion(champ_dict, row['championId'])
-          participants_row['spell1'] = row['spell1Id']
-          participants_row['spell2'] = row['spell2Id']
+          participants_row['spell1'] = get_spell(spell_dict, row['spell1Id'])
+          participants_row['spell2'] = get_spell(spell_dict, row['spell2Id'])
           participants_row['win'] = row['stats']['win']
           participants_row['kills'] = row['stats']['kills']
           participants_row['deaths'] = row['stats']['deaths']
           participants_row['assists'] = row['stats']['assists']
           """ participants_row['totalDamageDealt'] = row['stats']['totalDamageDealt']
-          participants_row['goldEarned'] = row['stats']['goldEarned']
-          participants_row['champLevel'] = row['stats']['champLevel']
-          participants_row['totalMinionsKilled'] = row['stats']['totalMinionsKilled'] """
+          participants_row['goldEarned'] = row['stats']['goldEarned']"""
+          participants_row['level'] = row['stats']['champLevel']
+          participants_row['minionsKilled'] = row['stats']['totalMinionsKilled'] 
           participants_row['item0'] = get_item(item_dict, row['stats']['item0'])
           participants_row['item1'] = get_item(item_dict, row['stats']['item1'])
           participants_row['item2'] = get_item(item_dict, row['stats']['item2'])
           participants_row['item3'] = get_item(item_dict, row['stats']['item3'])
           participants_row['item4'] = get_item(item_dict, row['stats']['item4'])
           participants_row['item5'] = get_item(item_dict, row['stats']['item5'])
-          participants_row['item6'] = get_item(item_dict, row['stats']['item6'])
+          participants_row['trinket'] = get_item(item_dict, row['stats']['item6'])
           stats.append(participants_row)
      df1 = pd.DataFrame(participants)
      df2 = pd.DataFrame(stats)
-     #df3 = pd.DataFrame(full)
      print(df1)
      print(df2.to_string())
-     #print(df3)
-     
+     print(stats)
+     """insert = INSERT INTO Matches(gameId, participantId, summoner, champion, spell1, spell2, win, kills, deaths, assists, 
+     level, minionsKilled, item0, item1, item2, item3, item4, item5, trinket) 
+     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+     for row in stats:
+          summonerData = (row['gameId'], row['participantId'], row['summoner'], row['champion'], row['spell1'], row['spell2'],
+          row['win'], row['kills'], row['deaths'], row['assists'], row['level'], row['minionsKilled'], row['item0'],
+          row['item1'], row['item2'], row['item3'], row['item4'], row['item5'], row['trinket'])
+          cursor.execute(insert, summonerData) """
      return stats
 
 def main():
-     cursor = connect_to_database()
      lol_watcher = get_watcher()
      summoner = get_summoner(lol_watcher)
      champ_dict = get_champ_dict(lol_watcher)
      item_dict = get_item_dict(lol_watcher)
      spell_dict = get_spell_dict(lol_watcher)
-     stats = get_matchlist(lol_watcher, summoner, region, champ_dict, item_dict)
-     #get_champion(get_champ_dict(lol_watcher), stats)
+     index = 2
+     my_matchlist = get_matchlist(lol_watcher, region, summoner, index)
+     stats = get_match(lol_watcher, my_matchlist, champ_dict, item_dict, spell_dict)
+     cursor = add_to_database(stats)
+     
+     
 
 if __name__ == "__main__":
      main()
